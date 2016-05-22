bc9377b43955a160c5aafbf06d877d53b571bc74
David Gouldsmith <gouldsmith@users.noreply.github.com>
2016-05-22 13:36:31 -0400

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,5 @@
+puts “Hello World”
+puts “Hello Again”
+puts “I like typing this”
+puts “Yay! Printing”
+puts ‘I “said” do not touch this.’
---
+++ ex6.rb
@@ -0,0 +1,4 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
